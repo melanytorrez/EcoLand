c821da40e38e7daa3f67b7a4e305846cb0103cec
melanytorrez <torrezmelany13@gmail.com>
2026-03-15 21:15:55 -0400

    fix: correcting maven issues

---
+++ backend/src/main/java/com/ecoland/application/service/AuditoriaService.java
@@ -0,0 +1,8 @@
+package com.ecoland.application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuditoriaService {
+    // Pendiente de implementar por el equipo de auditoría
+}
